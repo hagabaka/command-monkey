37067436f84c7a479c0086ccfdb4255204cccd7b
Yaohan Chen <yaohan.chen@gmail.com>
2010-03-31 13:55:38 -0400

    use filter_output in irb-test

--- irb-test.rb
+++ irb-test.rb
@@ -3,6 +3,10 @@
 require 'command-monkey'
 
 irb = CommandMonkey.new('irb', /irb.*>/)
+def irb.filter_output(text)
+  super(text).sub(/\A=> /m, '')
+end
+
 p irb.command('1+3')
 p irb.command('"a".upcase')
 
